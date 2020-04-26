43694f01a91f84e38056f98cccd7013f597bc619
Michael Guan <mhg99@cornell.edu>
2020-04-25 23:50:31 -0400

    first commit

---
+++ src/sample/Blox.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class Blox {
+    String type;
+    String color;
+    public Blox(String type, String color) {
+       this.type = type;
+       this.color = color;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void printBlox() {
+        System.out.println(type.charAt(0));
+    }
+}
---
+++ src/sample/BloxWorld.java
@@ -0,0 +1,40 @@
+package sample;
+
+public class BloxWorld {
+    int width;
+    int height;
+    Blox[][] world;
+
+    public BloxWorld(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.world = new Blox[width][height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public Blox[][] getWorld() {
+        return world;
+    }
+    public void setBlox(int x, int y, Blox blox) {
+        world[x][y] = blox;
+    }
+    public void printWorld() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (world[i][j] == null) {
+                    System.out.print(0);
+                }
+                else {
+                    world[i][j].printBlox();
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
